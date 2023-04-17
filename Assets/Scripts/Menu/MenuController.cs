12010da390059874d5746a8f3c405c640f8574db
EduarXD <emateoma@uoc.edu>
2023-04-17 22:26:12 +0200

    Subida Alfa PEC 2 TFG
    Subida de un alfa probable para la PEC 2 del TFG

---
+++ Assets/Scripts/Menu/MenuController.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.EventSystems;
+
+public class MenuController : MonoBehaviour
+{
+    public TMP_FontAsset font_default;
+    public TMP_FontAsset font_dyslexic;
+    public bool use_dyslexic;
+    public EventSystem eventSystem;
+    [System.Serializable]
+    public struct SubMenu
+    {
+        public GameObject menu;
+        public UnityEngine.UI.Selectable defaultItem;
+    }
+    public uint current;
+    public SubMenu[] subMenus;
+    private void Start()
+    {
+        CheckFont();
+        for (int i = 0; i < subMenus.Length; i++)
+        {
+            subMenus[i].menu.SetActive(false);
+        }
+        ChangeMenu((int)current);
+    }
+    public void ChangeMenu(int index)
+    {
+        subMenus[current].menu.SetActive(false);
+        current = (uint)Mathf.Clamp(index, 0, subMenus.Length - 1);
+        subMenus[current].menu.SetActive(true);
+        subMenus[current].defaultItem.Select();
+        eventSystem.firstSelectedGameObject = subMenus[current].defaultItem.gameObject;
+    }
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    public void ChangeDyslexic(bool use_dyslexic) { this.use_dyslexic = use_dyslexic; CheckFont(); }
+    public void ChangeFullscreen(bool fullscreen) { Screen.fullScreen = fullscreen; }
+    public void CheckFont()
+    {
+        TMP_Text[] texts = GetComponentsInChildren<TMP_Text>(true);
+        if (use_dyslexic)
+        {
+            foreach (TMP_Text text in texts)
+            {
+                text.font = font_dyslexic;
+            }
+        }
+        else
+        {
+            foreach (TMP_Text text in texts)
+            {
+                text.font = font_default;
+            }
+        }
+    }
+}
